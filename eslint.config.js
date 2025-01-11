c5e968467fe399762a250e780d713690b4311b69
Ed <casedu@gmail.com>
2025-01-11 15:47:53 -0600

    v.6.4.4

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
+// import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/dev-dist/**'],
+  },
+
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  // skipFormatting,
 ];
